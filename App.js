350d9aa5a36e594b32ab85b2fc1d65a0c8f9862a
Muhammad Safwan P K <62184316+sfwnmuhd@users.noreply.github.com>
2025-03-12 17:06:24 +0530

    ep2 done

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-// const root = document.getElementById("root");
-// const heading = document.createElement('h1');
-// heading.innerHTML = "hello world";
-// root.appendChild(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-
-
-
-
-const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]),React.createElement("div",{id:"child"},[React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")])]);
+const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"Nmaste React"),React.createElement("h2",{},"I'm h2 tag")]),React.createElement("div",{id:"child"},[React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")])]);
 
 const heading = React.createElement("h1",{id:"heading"},"Hello World from React");
 
