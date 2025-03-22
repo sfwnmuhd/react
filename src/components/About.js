cda6e189c8e57f7eef6a190be2fb3ab356b9795b
Muhammad Safwan P K <62184316+sfwnmuhd@users.noreply.github.com>
2025-03-22 10:40:50 +0530

    strated building user information below aboutus page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
+import User from "./User";
+
 const About = () => {
     return (
         <div className="about">
             <h1>About</h1>
             <h2>this is namaste react web series</h2>
+            <User/>
         </div>
     )
 }
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+const User = ()=>{
+    return(
+        <div className="user-card">
+            <h2>Name: Safwan</h2>
+            <h3>Location: Kerala</h3>
+            <h4>Contact : @sfwnmuhd</h4>
+        </div>
+    )
+}
+
+
+export default User;
