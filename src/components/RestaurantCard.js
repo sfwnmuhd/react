7dc3a5877f80f6579da47da001c17e17bddd332a
Muhammad Safwan P K <62184316+sfwnmuhd@users.noreply.github.com>
2025-03-25 22:00:25 +0530

    restaurant card design using tailwind css

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -11,12 +11,15 @@ const RestuarantCard = (props)=>{
         costForTwo
     } = resData?.info;
     return(
-        <div className="m-4 p-4 w-[250px] border hover:bg-gray-400 hover:text-white h-[550px]" >
-            <img className="rounded-lg" src={CDN_URL + resData.info.cloudinaryImageId} ></img>
-            <h3 className="font-bold py-4 text-lg" >{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRating} stars</h4>
-            <h4>{sla?.slaString} minutes</h4>
+        <div className="m-4 p-4 w-[250px]  hover:bg-gray-400 hover:text-white " >
+            <img className="rounded-lg h-60 w-90" src={CDN_URL + resData.info.cloudinaryImageId} ></img>
+            <h3 className="font-bold py-2 text-lg" >{name}</h3>
+            <div className="flex font-medium">
+                <h4>⭐{avgRating}</h4>
+                <h4 className="pl-3">{sla?.slaString}</h4>
+            </div>
+            <h4 className="text-[15px] text-gray-400">{cuisines.join(", ")}</h4>
+            
             <h4>{costForTwo}</h4>
         </div>
     )
